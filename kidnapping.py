0744b06afa6c7a791e80b8733fc8c4987f1fc4da
harkirat2395 <harkirat2395@gmail.com>
2024-11-18 11:12:05 +0530

    kidnapping.py

--- kidnapping.py
+++ kidnapping.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 from streamlit_option_menu import option_menu
 import warnings
